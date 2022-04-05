08afb7e8152fd03a3b9f13ff5ccc70ddb555d1d8
omid <o.beygi@gmail.com>
2022-04-05 12:03:42 +0430

    4/5

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { calculateTotals } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
 }
 export default App;
